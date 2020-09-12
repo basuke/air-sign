32099e6df0993db78e6863b17b5c5904cf7396c5
Basuke Suzuki <basuke@mac.com>
2020-09-12 11:52:34 -0700

    http server

---
+++ main.js
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2020 Basuke
+*/
+
+import MDNS from "mdns";
+import {Server} from "http"
+import Net from "net"
+import config from "mc/config";
+
+let hostName = config.hostname ?? "air-sign";
+const port = config.port ? parseInt(config.port) : 80;
+const verbose = 'verbose' in config ? !!config.verbose : true;
+
+if (!port) {
+    trace(`Invalid port number: ${config.port}\n`);
+    return;
+}
+
+new MDNS({hostName}, (message, value) => {
+    if (message === MDNS.hostName)
+        hostName = value;
+});
+
+class Request {
+    constructor(method, path) {
+        this.method = method;
+        this.path = path;
+        this.contentType = "application/octed";
+        this.body = undefined;
+    }
+
+    receiveHeader(name, value) {
+        switch (name.toLowerCase()) {
+            case 'content-type':
+                this.contentType = value;
+                break;
+        }
+    }
+
+    log() {
+        trace(`${this.method} ${this.path}\n`);
+    }
+};
+
+class SignServer extends Server {
+    constructor(dictionary) {
+        super(dictionary);
+
+        let request = undefined;
+
+        this.callback = (message, arg1, arg2) => {
+            switch (message) {
+                case Server.status:
+                    request = new Request(arg2, arg1);
+                    break;
+
+                case Server.header:
+                    request.receiveHeader(arg1, arg2);
+                    break;
+
+                case Server.requestComplete:
+                    request.body = arg1;
+                    break;
+
+                case Server.prepareResponse:
+                    if (verbose) request.log();
+                    return this.onRequest(request);
+
+                case Server.responseComplete:
+                    request = undefined;
+                    break;
+            }
+        };
+    }
+
+    counter = 0;
+
+    onRequest({method, path, contentType, body}) {
+        const ip = Net.get("IP");
+        const ssid = Net.get("SSID");
+
+        return {
+            headers: ["Content-Type", "text/plain"],
+            body: `Client requested path ${path}.
+Request #${++this.counter} to this server.
+Server host name "${hostName}.local" at address ${ip} on network "${ssid}".
+`,
+        };
+    }
+};
+
+const server = new SignServer({port});
