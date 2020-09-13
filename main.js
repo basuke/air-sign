1838eff61d8d74c1e9209174817017dcb4c4a1fe
Basuke Suzuki <basuke@mac.com>
2020-09-13 13:00:17 -0700

    Added hostname display using poco

--- main.js
+++ main.js
@@ -5,6 +5,9 @@
 import Net from "net"
 import config from "mc/config";
 import { HandyServer, textResponse, registerHostName } from "network";
+import Poco from "commodetto/Poco";
+import Resource from "Resource";
+import parseBMF from "commodetto/parseBMF";
 
 const port = config.port ? parseInt(config.port) : 80;
 if (!port) {
@@ -12,11 +15,19 @@ if (!port) {
     return;
 }
 
+let render = new Poco(screen, { displayListLength: 2048 });
+let font = parseBMF(new Resource("OpenSans-Semibold-18.bf4"));
+let black = render.makeColor(0, 0, 0);
+let white = render.makeColor(255, 255, 255);
+
 let hostName = undefined;
 
 registerHostName(config.hostname ?? "air-sign", value => {
     hostName = value;
+    showHostName();
 });
+
+showHostName();
 
 const server = new HandyServer({
     port,
@@ -28,3 +39,13 @@ server.onRequest = ({method, path, contentType, body}) => {
     const ssid = Net.get("SSID");
     return textResponse(`Client requested path ${path}. Server host name "${hostName}.local" at address ${ip} on network "${ssid}".\n`);
 };
+
+function showHostName() {
+    render.begin();
+        render.fillRectangle(black, 0, 0, render.width, render.height);
+        const text = 'http://' + (hostName ? hostName + '.local' : Net.get("IP"));
+        render.drawText(text, font, white,
+            (render.width - render.getTextWidth(text, font)) >> 1,
+            (render.height - font.height) >> 1);
+    render.end();
+}
