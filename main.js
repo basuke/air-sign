002fd2c8c482ee8e2cc2db74f5bfd439040d81d2
Basuke Suzuki <basuke@mac.com>
2020-09-13 00:25:29 -0700

    Remove UI because of the memory

--- main.js
+++ main.js
@@ -6,22 +6,22 @@ import MDNS from "mdns";
 import {Server} from "http"
 import Net from "net"
 import config from "mc/config";
-import {} from "piu/MC";
-import Timer from "timer";
 
-let hostName = config.hostname ?? "air-sign";
 const port = config.port ? parseInt(config.port) : 80;
-const verbose = 'verbose' in config ? !!config.verbose : true;
-
 if (!port) {
     trace(`Invalid port number: ${config.port}\n`);
     return;
 }
 
+const ip = Net.get("IP");
+const ssid = Net.get("SSID");
+const verbose = 'verbose' in config ? !!config.verbose : true;
+
+let hostName = config.hostname ?? "air-sign";
+
 new MDNS({hostName}, (message, value) => {
     if (message === MDNS.hostName)
         hostName = value;
-        Timer.set(() => app.first.string = hostName, 100);
 });
 
 class Request {
@@ -79,44 +79,11 @@ class SignServer extends Server {
     counter = 0;
 
     onRequest({method, path, contentType, body}) {
-        return { headers: ["Content-Type", "text/plain"], body: "OK" };
+        return {
+            headers: ["Content-Type", "text/plain"],
+            body: `Client requested path ${path}. Server host name "${hostName}.local" at address ${ip} on network "${ssid}".`,
+        };
     }
 };
 
 const server = new SignServer({port});
-
-/* UI */
-
-const bgSkin = new Skin({ fill:"black" });
-const textStyle = new Style({ font:"OpenSans-Semibold-28", left:10, right:10, top:15, bottom:15, color: "white" });
-const centerStyle = new Style({ horizontal:"center" });
-
-const UIApp = Application.template($ => ({
-    skin: bgSkin,
-    style: textStyle,
-	contents: [
-		Text($, { 
-            anchor: "TEXT",
-            left: 0,
-            right: 0,
-            top: 0,
-            style: centerStyle, 
-            active:true,
-			string: hostName,
-		}),
-	]
-}));
-
-const app = new UIApp(null, { displayListLength:4608 });
-const ip = Net.get("IP");
-const ssid = Net.get("SSID");
-
-server.onRequest = ({method, path, contentType, body}) => {
-    app.first.string = path;
-    return {
-        headers: ["Content-Type", "text/plain"],
-        body: `Client requested path ${path}. Server host name "${hostName}.local" at address ${ip} on network "${ssid}".`,
-    };
-};
-
-export default app;
